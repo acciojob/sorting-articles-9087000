f07798dcdca5ee4b28142de7bf52532bf8b836b0
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-12 21:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
+//your JS code here. If required.
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-const ignoreWords = ['a','an','the'];
-
-function compareArticles(article1,article2) {
-    const comArticle1 = article1.toLowerCase().split(' ').filter(word => !ignoreWords.includes(word)).join(' ');	
-    const comArticle2 = article2.toLowerCase().split(' ').filter(word => !ignoreWords.includes(word)).join(' ');
-
-	if(comArticle1 < comArticle2){
-		return -1;
-	} else if(comArticle1 > comArticle2){
-		return 1;
-	}else{
-		return 0;
-	}
+function stripArticle(bandName) {
+    return bandName.replace(/^(a |an |the )/i, '').trim();
 }
 
-const sortedBands = bands.sort(compareArticles);
-// alert(sortedBands)
+const sortedBands = bands.sort((a, b) => stripArticle(a).localeCompare(stripArticle(b)));
 
 const ul = document.getElementById('bands');
-sortedBands.forEach((bands) => {
-	const listItems = document.createElement('li');
-	listItems.textContent = bands;
-	ul.appendChild(listItems);
+
+sortedBands.forEach(band => {
+    const li = document.createElement('li');
+    li.textContent = band;
+    ul.appendChild(li);
 });
